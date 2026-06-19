369565164a3eb575f6cafac12395445aa6b6e3ef
Aadi Jain <aadijaintikamgarh@gmail.com>
2026-06-20 03:11:50 +0530

    add .env.sample

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,10 +1,14 @@
 import { Redis } from "ioredis";
+import "dotenv/config";
 
-const redisClient = new Redis({
-    host: "localhost",
-    port: 6379,
-    password: "",
-    db: 0
+const redisClient = new Redis(process.env.REDIS_URL!)
+
+redisClient.on("connect", () => {
+    console.log("Redis connected");
+})
+
+redisClient.on("error", (error) => {
+    console.log("Redis error: ", error);
 })
 
 export { redisClient };
